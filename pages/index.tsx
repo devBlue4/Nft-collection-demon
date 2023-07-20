c79fee6f0354697d829c55852a8a41bb63e4b98a
devBlue4 <137197944+devBlue4@users.noreply.github.com>
2023-07-19 21:53:33 -0300

    Update index.tsx
    se modifico la linea 78 y 79

--- pages/index.tsx
+++ pages/index.tsx
@@ -76,8 +76,7 @@ const Home: NextPage = () => {
           <div className={styles.heroSection}>
             <div className={styles.collectionImage}>
               <MediaRenderer
-                src={contractMetadata.image}
-              />
+                src={contractMetadata.image}/>
             </div>
             <div>
               <h1>{contractMetadata.name}</h1>             
